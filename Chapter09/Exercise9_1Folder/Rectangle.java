04b28519f524f1036ff46b853b5069e1e96d2d0c
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2022-10-05 19:03:15 -0500

    Exercise09_01Folder

---
+++ Chapter09/Exercise9_1Folder/Rectangle.java
@@ -0,0 +1,22 @@
+package Chapter09.Exercise9_1Folder;
+
+public class Rectangle {
+	double width;	// Width of rectangle
+	double height;	// Height of rectangle
+
+	public Rectangle(double width, double height) {
+		this.width = width;
+		this.height = height;
+	}
+	Rectangle() {
+		width = 1;
+		height = 1;
+	} 
+	double getArea() {
+		return width * height; 
+	}
+	double getPerimeter() {
+		return 2 * (width + height);
+	}
+}
+
