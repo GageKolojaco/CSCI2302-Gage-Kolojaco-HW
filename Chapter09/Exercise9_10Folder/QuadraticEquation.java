8c55e872c2f319db80b61ce96c236c26aba5dcce
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2022-10-05 22:57:37 -0500

    final fixes3

--- Chapter09/Exercise9_10Folder/QuadraticEquation.java
+++ Chapter09/Exercise9_10Folder/QuadraticEquation.java
@@ -1,32 +1,93 @@
-package Chapter09.Exercise9_10Folder;
+import java.util.Scanner;
 
+public class Exercise_09_10 {
+	/** Main method */
+	public static void main(String[] args) {
+		// Create a Scanner object
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user to enter values for a, b, and c
+		System.out.print("Enter a, b, c: ");
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
+
+		// Create a QuadraticEquation object
+		QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+
+		// Compute the real roots of the quadriatic equation if any.
+		System.out.print("The equation has ");
+		if (quadraticEquation.getDiscriminant() < 0)
+			System.out.println("no real roots");
+		else if (quadraticEquation.getDiscriminant() > 0) {
+			System.out.println("two roots " + quadraticEquation.getRoot1() + 
+				" and " + quadraticEquation.getRoot2());
+		}
+		else {
+			System.out.println("one root " + (quadraticEquation.getRoot1() > 0 ? 
+				quadraticEquation.getRoot1() : quadraticEquation.getRoot2()));
+		}
+	}
+}
+
+/*******************************************************
+*                 QuadraticEquation                    *
+--------------------------------------------------------
+* -a: double                                           *
+* -b: double                                           *
+* -c: double                                           *
+* +QuadradticEquation(a: double, b: double, c: double) *
+* +getA(): double                                      *
+* +getB(): double                                      *
+* +getC(): double                                      *
+* +getDiscriminant(): double                           *
+* +getRoot1(): double                                  *
+* +getRoot2(): double                                  *
+*******************************************************/
+
+// Implement QuadradticEquation class
 public class QuadraticEquation {
+	/** Data fields */
+	// represent three coeficients
+	private double a;
+	private double b;
+	private double c;
 
-	public double a;
-	public double b;
-	public double c;
-
+	/** Constructor for the arguments for a, b, and c */
 	QuadraticEquation(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
+
+	/** Accessor Methods */
+	/** Returns a */
 	public double getA() {
 		return a;
 	}
+
+	/** Returns b */
 	public double getB() {
 		return b;
 	}
+
+	/** Returns c */
 	public double getC() {
 		return c;
 	}
+
+	/** Returns the discriminant */
 	public double getDiscriminant() {
 		return Math.pow(b, 2) - 4 * a * c;
 	}
+
+	/** Returns root1 */
 	public double getRoot1() {
 		return getDiscriminant() < 0 ? 0 :
 			((-b) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
 	}
+
+	/** Returns root2 */
 	public double getRoot2() {
 		return getDiscriminant() < 0 ? 0 :
 			((-b) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
